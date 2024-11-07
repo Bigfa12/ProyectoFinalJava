c22f5751e23ac162dc5308bfc12f85f08c657a4f
Bigfa12 <milrayas7@gmail.com>
2024-11-07 14:54:01 -0300

    Last Update

--- untitled/src/Exeption/NombreVacioExeption.java
+++ untitled/src/Exeption/NombreVacioExeption.java
@@ -2,6 +2,6 @@ package Exeption;
 
 public class NombreVacioExeption extends RuntimeException {
     public NombreVacioExeption() {
-        super("El nombre no puede ser vacio");
+        super("El nombre no puede estar vacio");
     }
 }
